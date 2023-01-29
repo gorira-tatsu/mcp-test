9dbca0fa48c9c1fdaf8e385a829f6e57aceb5493
gorira-tatsu <tatsuhiko.shigoto@gmail.com>
2023-01-30 01:43:25 +0900

    fix schema and wrote sample code

--- docker/mysql/initdb.d/schema.sql
+++ docker/mysql/initdb.d/schema.sql
@@ -5,3 +5,9 @@ CREATE TABLE users (
   email VARCHAR(30) UNIQUE NOT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO users (username, password, email) values ('gorira', 'gorira', 'gorira@example.com')
+INSERT INTO users (username, password, email) values ('panda', 'panda', 'panda@example.com')
+INSERT INTO users (username, password, email) values ('rabbit', 'rabbit', 'rabbit@example.com')
+INSERT INTO users (username, password, email) values ('dog', 'dog', 'dog@example.com')
+INSERT INTO users (username, password, email) values ('cat', 'cat', 'cat@example.com')
