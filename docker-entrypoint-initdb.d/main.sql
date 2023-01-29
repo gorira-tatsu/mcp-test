0e346bf330803151b0a294c322472c3c96ff3224
gorira-tatsu <tatsuhiko.shigoto@gmail.com>
2023-01-30 00:09:24 +0900

    fix schema and wrote sample code

--- docker-entrypoint-initdb.d/main.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-  num int NOT NULL AUTO_INCREMENT,
-  username varchar(20) UNIQUE NOT NULL,
-  password varchar(64) NOT NULL,
-  email varchar(30) UNIQUE NOT NULL,
-  primary key (num)
-);
--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 import mysql.connector
 
 connect = mysql.connector.connect(host='db', database='test', user='test', password='test')
-cur = connect.cursor()
+cur = connect.cursor(buffered=True)
+# If you want to get it in dictionary type, write dictionary=True as an argument
+# example cur = connect.cursor(dictionary=True)
 
+# If test data is not inserted, insert it.
+try:
+    cur.execute(
+        'select * from users'
+    )
+    if cur.fetchone() is None:
+        cur.execute("INSERT INTO users (username, password, email) values ('gorira', 'gorira', 'gorira@example.com')")
+        cur.execute("INSERT INTO users (username, password, email) values ('panda', 'panda', 'panda@example.com')")
+        cur.execute("INSERT INTO users (username, password, email) values ('rabbit', 'rabbit', 'rabbit@example.com')")
+        cur.execute("INSERT INTO users (username, password, email) values ('dog', 'dog', 'dog@example.com')")
+        cur.execute("INSERT INTO users (username, password, email) values ('cat', 'cat', 'cat@example.com')")
+        connect.commit()
+except:
+    cur.rollback()
 
-cur.execute(
-    ''
-)
+# Sample code starts here.
+try:
+    cur.execute('SELECT * FROM users')
+    print(cur.fetchone())
+    print(cur.fetchmany(2))
+    print(cur.fetchall())
+except:
+    pass
 
+try:
+    cur.execute("INSERT INTO users (username,password,email) VALUES ('gorira','gorira','gorira@examle.com')")
+    connect.commit()
+except mysql.connector.errors.IntegrityError:
+    print("すでに挿入されています")
